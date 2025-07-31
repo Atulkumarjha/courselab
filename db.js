48536ca9ab4f7303cb9090e9a1cd60dd76cbf9ff
Atul kumar jha <jhaak301@gmail.com>
2025-07-31 23:12:20 +0530

    MongoDB connection

---
+++ db.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://courselab:courselab@courselab.vjczqhe.mongodb.net/");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const adminSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+});
+
+const purchaseSchema = Schema({
+    userId: ObjectId,
+    courseId: ObjectId
+});
+
+const userModel = mongoose.Model("user", userSchema);
+const adminModel = mongoose.Model("admin", adminSchema);
+const courseModel = mongoose.Model("course", courseSchema);
+const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+
+module.export {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+};
